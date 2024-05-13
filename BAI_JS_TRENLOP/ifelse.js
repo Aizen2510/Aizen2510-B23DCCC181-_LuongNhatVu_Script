c339c138cfa25a5cce86c7aefa9800a02865f118
Aizen <vu09097@gmail.com>
2024-05-13 14:50:16 +0700

    Aizen

---
+++ BAI_JS_TRENLOP/ifelse.js
@@ -0,0 +1,38 @@
+
+a=prompt('nhập vào số a:')
+b=prompt('nhập vào số b:')
+if (a==2) {
+    alert('Hôm nay là thứ 2')
+}
+else if (a==3) {
+    alert('Hôm nay là thứ 3')
+}
+else if (a==4) {
+    alert('Hôm nay là thứ 4')
+}
+else if (a==5) {
+    alert('Hôm nay là thứ năm')
+}
+else if (a==6) {
+    alert('Hôm nay là thứ sáu')
+}
+else if (a==7) {
+    alert('Hôm nay là thứ bảy')
+}
+else if (a==8) {
+    alert('Hôm nay là chủ nhật')
+}
+else {
+    alert('Error')
+}
+
+if (a<b) {
+    alert('Chúc Bạn 1 ngày không cóa bug :(( ')
+}
+else if (a==b) {
+    alert('Chúc Bạn ngày tốt lành ')
+}
+else {
+    alert('CÓ bug rồi')
+}
+
---
+++ BAI_JS_TRENLOP/swith.js
@@ -0,0 +1,30 @@
+let day = prompt('Nhập vào số số bạn muốn:');
+
+
+day = parseInt(day);
+
+switch (day) {
+    case 2:
+        document.write("Hôm nay là thứ ba");
+        break;
+    case 3:
+        document.write("Hôm nay là thứ ba");
+        break;
+    case 4:
+        document.write("Hôm nay là thứ tư");
+        break;
+    case 5:
+        document.write("Hôm nay là thứ năm");
+        break;
+    case 6:
+        document.write("Hôm nay là thứ sáu");
+        break;
+    case 7:
+        document.write("Hôm nay là thứ bảy");
+        break;
+    case 8:
+        document.write("Hôm nay là chủ nhật");
+        break;
+    default:
+        document.write("Lỗi rồi");          
+}
